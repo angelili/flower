f2380a2427ac7de526356cfa3b66278f4025bf48
angelili <99340194+angelili@users.noreply.github.com>
2023-06-01 16:49:10 +0200

    Update run.sh

--- examples/pytorch_from_centralized_to_federated/run.sh
+++ examples/pytorch_from_centralized_to_federated/run.sh
@@ -8,7 +8,7 @@ srun server.py
 
 # Loop through the client job names and submit each client job to the "cuda" partition
 for client in "${clients[@]}"; do
-    srun --partition=cuda --nodes=1 --ntasks=1 --exclusive client.py "$client" &
+    sbatch client.py &
 done
 
 # Wait for all background client jobs to finish
