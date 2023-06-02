9e671d0d6946859c95904d39d3f3acb1f27f2aa9
angelili <99340194+angelili@users.noreply.github.com>
2023-06-02 14:52:12 +0200

    Update server.py

--- examples/pytorch_from_centralized_to_federated/server.py
+++ examples/pytorch_from_centralized_to_federated/server.py
@@ -5,6 +5,6 @@ import flwr as fl
 
 if __name__ == "__main__":
     fl.server.start_server(
-        server_address="0.0.0.0:8080",
+        server_address="10.30.0.8:6379",
         config=fl.server.ServerConfig(num_rounds=3),
     )
