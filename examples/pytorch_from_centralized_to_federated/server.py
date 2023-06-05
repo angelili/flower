199a9f3ca20bba80308087b5aa65f631e1165ccb
angelili <99340194+angelili@users.noreply.github.com>
2023-06-05 12:36:55 +0200

    Update server.py

--- examples/pytorch_from_centralized_to_federated/server.py
+++ examples/pytorch_from_centralized_to_federated/server.py
@@ -3,13 +3,14 @@ fedl_server="10.30.0.254:9000"
 fedl_no_proxy=True
 import flwr as fl
 
-if fedl_no_proxy:
-   import os
-   os.environ["http_proxy"] = ""
-   os.environ["https_proxy"] = ""
+
 
 if __name__ == "__main__":
+    if fedl_no_proxy:
+      import os
+      os.environ["http_proxy"] = ""
+      os.environ["https_proxy"] = ""
     fl.server.start_server(
-        server_address= "10.30.0.254:6817",
+        server_address= "10.30.0.254:9000",
         config=fl.server.ServerConfig(num_rounds=3),
     )
