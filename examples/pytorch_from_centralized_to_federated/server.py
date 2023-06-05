36d3f5614ebfdbc61b1ed5d81ed86dbcb0ad1e1c
angelili <99340194+angelili@users.noreply.github.com>
2023-06-05 12:54:21 +0200

    Update server.py

--- examples/pytorch_from_centralized_to_federated/server.py
+++ examples/pytorch_from_centralized_to_federated/server.py
@@ -1,14 +1,13 @@
 """Flower server example."""
-fedl_server="10.30.0.254:9000"
+
 
 import flwr as fl
-
+import os
 
 
 if __name__ == "__main__":
     fedl_no_proxy=True
     if fedl_no_proxy:
-      import os
       os.environ["http_proxy"] = ""
       os.environ["https_proxy"] = ""
     fl.server.start_server(
