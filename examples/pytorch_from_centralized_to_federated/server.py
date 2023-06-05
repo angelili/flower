87b3470bd827f1ec031aa09425a3e7f0ff6d6577
angelili <99340194+angelili@users.noreply.github.com>
2023-06-05 10:51:42 +0200

    Update server.py

--- examples/pytorch_from_centralized_to_federated/server.py
+++ examples/pytorch_from_centralized_to_federated/server.py
@@ -10,6 +10,6 @@ if fedl_no_proxy:
 
 if __name__ == "__main__":
     fl.server.start_server(
-        server_address= fedl_server,
+        server_address= "0.0.0.0:8080",
         config=fl.server.ServerConfig(num_rounds=3),
     )
