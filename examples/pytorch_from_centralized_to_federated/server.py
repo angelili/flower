fa03959cd35ee775f5df650d1910972bbecf4493
angelili <99340194+angelili@users.noreply.github.com>
2023-06-14 07:44:37 +0200

    Update server.py

--- examples/pytorch_from_centralized_to_federated/server.py
+++ examples/pytorch_from_centralized_to_federated/server.py
@@ -4,6 +4,40 @@
 import flwr as fl
 import os
 
+def get_evaluate_fn(
+    testset: torchvision.datasets.CIFAR10,
+) -> Callable[[fl.common.NDArrays], Optional[Tuple[float, float]]]:
+    """Return an evaluation function for centralized evaluation."""
+
+    def evaluate(
+        server_round: int, parameters: fl.common.NDArrays, config: Dict[str, Scalar]
+    ) -> Optional[Tuple[float, float]]:
+        """Use the entire CIFAR-10 test set for evaluation."""
+
+        # determine device
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+        model = Net()
+        set_params(model, parameters)
+        model.to(device)
+
+        testloader = torch.utils.data.DataLoader(testset, batch_size=50)
+        loss, accuracy = test(model, testloader, device=device)
+
+        # return statistics
+        return loss, {"accuracy": accuracy}
+
+    return evaluate
+    # configure the strategy
+    strategy = fl.server.strategy.FedAvg(
+        fraction_fit=0.1,
+        fraction_evaluate=0.1,
+        min_fit_clients=10,
+        min_evaluate_clients=10,
+        min_available_clients=pool_size,  # All clients should be available
+        on_fit_config_fn=fit_config,
+        evaluate_fn=get_evaluate_fn(testset),  # centralised evaluation of global model
+    )
 
 if __name__ == "__main__":
     fedl_no_proxy=True
