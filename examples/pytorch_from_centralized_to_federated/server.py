4c428246c780fa3cc3385ace6391e9cf4d4d1090
angelili <99340194+angelili@users.noreply.github.com>
2023-06-14 08:06:29 +0200

    Update server.py

--- examples/pytorch_from_centralized_to_federated/server.py
+++ examples/pytorch_from_centralized_to_federated/server.py
@@ -48,4 +48,5 @@ if __name__ == "__main__":
     fl.server.start_server(
         server_address= "10.30.0.254:9000",
         config=fl.server.ServerConfig(num_rounds=3),
+        strategy=strategy
     )
