bdd7924a3fb4e6d4e5d1d098d2115ce6dc507f04
angelili <99340194+angelili@users.noreply.github.com>
2023-06-14 09:22:58 +0200

    Update server.py

--- examples/pytorch_from_centralized_to_federated/server.py
+++ examples/pytorch_from_centralized_to_federated/server.py
@@ -40,7 +40,7 @@ if __name__ == "__main__":
     if fedl_no_proxy:
       os.environ["http_proxy"] = ""
       os.environ["https_proxy"] = ""
-      
+    DATA_ROOT_SERVER = "./server_dataset"
     testset = MNIST(DATA_ROOT_SERVER, train=False, download=True, transform=transform)
         # configure the strategy
     strategy = fl.server.strategy.FedAvg(
