4a8df939fd4c4d89bef3ffa52fca5043acc4906e
angelili <99340194+angelili@users.noreply.github.com>
2023-06-19 11:04:14 +0200

    Update cifar.py

--- examples/pytorch_from_centralized_to_federated/cifar.py
+++ examples/pytorch_from_centralized_to_federated/cifar.py
@@ -26,33 +26,152 @@ import random
 import torch
 
 
-
-
 DATA_ROOT = "./dataset"
-
+Dominant_class=False
+Non_uniform_cardinality=True
 
 def load_data() -> (
     Tuple[torch.utils.data.DataLoader, torch.utils.data.DataLoader, Dict]):
-    """Load CIFAR-10 (training and test set)."""
+    """Load MNIST (training and test set)."""
     transform = transforms.Compose(
-        [transforms.ToTensor(), transforms.Normalize((0.5), (0.5))]
+        [transforms.ToTensor(), transforms.Normalize((0.1307), (0.3081))]
     )
+    # Load the MNIST dataset
     trainset = MNIST(DATA_ROOT, train=True, download=True, transform=transform)
-
-    sample_size=500
-    indices = random.sample(range(len(trainset)), sample_size)
-    sampler = torch.utils.data.SubsetRandomSampler(indices)
-
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=16, shuffle=False, sampler=sampler)
-
-    testset = MNIST(DATA_ROOT, train=False, download=True, transform=transform)
-    sample_size=100
-    indices = random.sample(range(len(testset)), sample_size)
-    sampler = torch.utils.data.SubsetRandomSampler(indices)
-
-    testloader = torch.utils.data.DataLoader(testset, batch_size=16, shuffle=False, sampler=sampler)
-    num_examples = {"trainset": len(trainset), "testset": len(testset)}
     
+    testset = MNIST(DATA_ROOT, train=False, download=True, transform=transform)
+
+    if Dominant_class==True:
+  
+
+        # Define the class to include 80% of samples
+        class_to_include = random.randint(0, 9)  # Change this to the desired class
+
+        # Get the indices of samples belonging to the specified class
+        class_indices = torch.where(trainset.targets == class_to_include)[0]
+
+        # Convert to np.array
+        class_indices=class_indices.numpy()
+
+        # Calculate the number of samples to include from the specified class
+        num_samples_class = int(len(class_indices) * 0.8)
+
+        # Shuffle the set of samples from the specified class
+        np.random.shuffle(class_indices)
+
+        # Select the first `num_samples_class` elements
+        subset_indices_class = class_indices[:num_samples_class]
+
+        # Convert back to tensor
+        subset_indices_class=torch.from_numpy(subset_indices_class)
+
+        # Create a Subset of the original dataset using the selected subset indices from the specified class
+        subset_dataset_class = torch.utils.data.Subset(trainset, subset_indices_class)
+
+        # Calculate the number of samples to include from other classes
+        num_samples_other = int(len(trainset) * 0.2)
+
+        # Get the indices of samples from other classes
+        other_indices = torch.where(trainset.targets != class_to_include)[0]
+
+        # Convert to np.array
+        other_indices=other_indices.numpy()
+
+        # Shuffle the set of samples from other classes
+        np.random.shuffle(other_indices)
+
+        # Select the first `num_samples_others` elements
+        subset_indices_other = other_indices[:num_samples_class]
+
+        # Convert back to tensor
+        subset_indices_other=torch.from_numpy(subset_indices_other)
+
+        # Create a Subset of the original dataset using the selected subset indices from other classes
+        subset_dataset_other = torch.utils.data.Subset(trainset, subset_indices_other)
+
+        # Concatenate the subsets from the specified class and other classes
+        trainset = torch.utils.data.ConcatDataset([subset_dataset_class, subset_dataset_other])
+
+        # Create the DataLoader with the specified subsets
+        trainloader = torch.utils.data.DataLoader(trainset, batch_size=32, shuffle=True)
+
+
+
+
+        # Define the class to include 80% of samples
+        class_to_include = random.randint(0, 9)  # Change this to the desired class
+
+        # Get the indices of samples belonging to the specified class
+        class_indices = torch.where(testset.targets == class_to_include)[0]
+
+        # Convert to np.array
+        class_indices=class_indices.numpy()
+
+        # Calculate the number of samples to include from the specified class
+        num_samples_class = int(len(class_indices) * 0.8)
+
+        # Shuffle the set of samples from the specified class
+        np.random.shuffle(class_indices)
+
+        # Select the first `num_samples_class` elements
+        subset_indices_class = class_indices[:num_samples_class]
+
+        # Convert back to tensor
+        subset_indices_class=torch.from_numpy(subset_indices_class)
+
+        # Create a Subset of the original dataset using the selected subset indices from the specified class
+        subset_dataset_class = torch.utils.data.Subset(testset, subset_indices_class)
+
+        # Calculate the number of samples to include from other classes
+        num_samples_other = int(len(testset) * 0.2)
+
+        # Get the indices of samples from other classes
+        other_indices = torch.where(testset.targets != class_to_include)[0]
+
+        # Convert to np.array
+        other_indices=other_indices.numpy()
+
+        # Shuffle the set of samples from other classes
+        np.random.shuffle(other_indices)
+
+        # Select the first `num_samples_others` elements
+        subset_indices_other = other_indices[:num_samples_class]
+
+        # Convert back to tensor
+        subset_indices_other=torch.from_numpy(subset_indices_other)
+
+        # Create a Subset of the original dataset using the selected subset indices from other classes
+        subset_dataset_other = torch.utils.data.Subset(testset, subset_indices_other)
+
+        # Concatenate the subsets from the specified class and other classes
+        testset = torch.utils.data.ConcatDataset([subset_dataset_class, subset_dataset_other])
+
+        # Create the DataLoader with the specified subsets
+        testloader = torch.utils.data.DataLoader(testset, batch_size=32, shuffle=True)
+        num_examples = {"trainset": len(trainset), "testset": len(testset)}
+
+        return trainloader, testloader, testset, num_examples
+    
+    if Non_uniform_cardinality==True:
+        sample_size_train = random.randint(200, 1000)
+        sample_size_test =  int(sample_size_train*0.1)
+    else:
+        sample_size_train=500
+        sample_size_test=100
+
+    indices_train = random.sample(range(len(trainset)), sample_size_train)
+    sampler_train= torch.utils.data.SubsetRandomSampler(indices_train)
+
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=16, shuffle=False, sampler=sampler_train)
+    print(len(trainloader))
+    print('dugi',sample_size_train)
+    print('kraci', sample_size_test)
+    indices_test = random.sample(range(len(testset)), sample_size_test)
+    sampler_test = torch.utils.data.SubsetRandomSampler(indices_test)
+
+    testloader = torch.utils.data.DataLoader(testset, batch_size=16, shuffle=False, sampler=sampler_test)
+    num_examples = {"trainset": len(trainset), "testset": len(testset)}
+
     return trainloader, testloader, testset, num_examples
 
 
